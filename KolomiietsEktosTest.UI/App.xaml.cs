f87fe23d970c57eebf9cd72ee71e3544f0af3860
Pavel <kolomiietspavlo881@gmail.com>
2025-08-12 19:15:21 +0300

    Add project files.

---
+++ KolomiietsEktosTest.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KolomiietsEktosTest.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ KolomiietsEktosTest.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KolomiietsEktosTest.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
