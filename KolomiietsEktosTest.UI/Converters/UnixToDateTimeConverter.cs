deddf67882017cde317240ae7245c3c0fba240f9
Pavel <kolomiietspavlo881@gmail.com>
2025-08-13 19:36:52 +0300

    unix converter

---
+++ KolomiietsEktosTest.UI/Converters/UnixToDateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace KolomiietsEktosTest.UI.Converters
+{
+    public class UnixToDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is uint unix)
+            {
+                var date = DateTimeOffset.FromUnixTimeSeconds(unix).LocalDateTime;
+                return date.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
